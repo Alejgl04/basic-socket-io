5ac763bd09c3a743e0482a8322be7e12de2bbd0b
Alejandro Guerrero <alejo.jesus.magne@gmail.com>
2022-04-19 19:59:31 -0700

    get id users by socket

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const { Router } = require('express');
 
 const router = Router();
@@ -46,6 +47,16 @@ router.post('/messages/:id', ( req = Request, res = Response ) => {
     id
   });
 });
+
+router.get('/users', async( req = Request, res = Response) => {
+  const clients  = (await req.io.fetchSockets()).map(socket => socket.id); 
+  return res.json({
+     ok:true,
+     clients 
+   });
+});
+
+
 
 
 module.exports = router;
